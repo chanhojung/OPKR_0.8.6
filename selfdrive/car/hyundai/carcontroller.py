d256cb7cea36d06d2d092e74061629c2fee2f5c1
Chanho Jung <jungch@naver.com>
2021-07-21 13:15:05 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -409,7 +409,7 @@ class CarController():
       #     self.cruise_gap_auto_switch_timer = 0
       #   if CS.cruiseGapSet == 1.0:
       #     self.cruise_gap_auto_switch_timer = 0
-      if (CS.out.vEgo * CV.MS_TO_KPH) >= 40 :# 시속 40킬로 이상 GAP_DIST 2칸 만들기
+      if (CS.out.vEgo * CV.MS_TO_KPH) > 30 :# 시속 30킬로 초과 GAP_DIST 2칸 만들기
         self.cruise_gap_auto_switch_timer += 1
         if self.cruise_gap_auto_switch_timer > 25 and (CS.cruiseGapSet != 2.0) :
           can_sends.append(create_clu11(self.packer, frame, CS.clu11, Buttons.GAP_DIST)) if not self.longcontrol \
